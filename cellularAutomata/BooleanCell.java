c22eeed935075c380a07fa57e6ef8cf7a8044b96
Anders <woxxey@gmail.com>
2012-03-06 21:28:52 +0000

    Old src files.

---
+++ cellularAutomata/BooleanCell.java
@@ -0,0 +1,14 @@
+/*
+ * Simple definition of an interface. The point is to allow my GameOfLife class to run on any 
+ * grid of Squares as long as they implement this.
+ */
+
+package cellularAutomata;
+
+public interface BooleanCell {
+	
+	public void setLive(boolean bool);
+	
+	public boolean isLive();
+	
+}
---
+++ cellularAutomata/SquareBooleanCell.java
@@ -0,0 +1,35 @@
+/*
+ * Extends ignotas square class to implement BooleanCell interface. I am thus able to treat it the 
+ * same way as any other class implementing the same interface.
+ */
+package cellularAutomata;
+
+public class SquareBooleanCell extends Square implements BooleanCell{
+	public SquareBooleanCell (int x, int y, int squareSize, int widthFrameBorder, int heightFrameBorder) {
+		super(x, y, squareSize, widthFrameBorder, heightFrameBorder);
+	}
+	
+	public SquareBooleanCell () {
+		super();
+	}
+
+	@Override
+	public void setLive(boolean bool) {
+		if (bool) {
+			changeColor(liveColor);
+		} else {
+			changeColor(deadColor);
+		}
+	}
+
+
+	@Override
+	public boolean isLive() {
+		if (getSquare().getFillColor() == liveColor) {
+			return true;
+		}else  { // Assume any non live square to be dead for now.
+			return false;
+		}
+	}
+	
+}
