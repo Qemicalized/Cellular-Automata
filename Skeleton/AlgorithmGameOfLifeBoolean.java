71e13e5f347756650f096bb09eec6c38794ee0b7
Anders <woxxey@gmail.com>
2012-03-07 20:45:19 +0000

    My initial suggestion for a code skeleton

---
+++ Skeleton/AlgorithmGameOfLifeBoolean.java
@@ -0,0 +1,8 @@
+
+public class AlgorithmGameOfLifeBoolean {
+	//lots of internal variables to keep track of things
+	//and lots of private methods that only the original coder REALLY need to know
+	
+	public void stepForward(InterfaceSomeOther[][] grid, int generations) {
+	}
+}
---
+++ Skeleton/InterfaceBooleanProperties.java
@@ -0,0 +1,11 @@
+/*
+ * One of many interfaces, this interface in particular ensures that bethods required to treat a
+ * Square similar a boolean value are implemented.
+ * 
+ * It is required by for instance a basic version of the game of life.
+ */
+public interface InterfaceBooleanProperties {
+	public void setBool(boolean bool);
+	public boolean getBool();
+	
+}
