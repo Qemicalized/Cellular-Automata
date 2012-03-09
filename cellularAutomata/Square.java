478f204ec456df3ea8503b23e611c5e7bd3abf64
Anders <woxxey@gmail.com>
2012-03-10 02:28:21 +0800

    restructuring file tree

---
+++ cellularAutomata/Square.java
@@ -0,0 +1,15 @@
+package cellularAutomata;
+
+public class Square{
+
+	public boolean getBool() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setBool(boolean bool) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
