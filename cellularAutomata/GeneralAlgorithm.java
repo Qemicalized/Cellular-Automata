61a4c500dc9b2594dca496891714e2c5ee032d57
Anders <woxxey@gmail.com>
2012-03-22 21:12:53 +0000

    removing old code

--- cellularAutomata/GeneralAlgorithm.java
+++
@@ -1,8 +0,0 @@
-/*
- * The class that contains everything that all algorithm classes must have in common
- */
-package cellularAutomata;
-
-public abstract class GeneralAlgorithm {
-	public abstract void advance(Square[][] grid, int count);
-}
--- testCode/TestGeneralAlgorithm.java
+++
@@ -1,8 +0,0 @@
-/*
- * The class that contains everything that all algorithm classes must have in common
- */
-package testCode;
-
-public abstract class TestGeneralAlgorithm {
-	public abstract void advance(TestSquare[][] grid, int count);
-}
