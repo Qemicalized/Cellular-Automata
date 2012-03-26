8991f7e98027b7069d6926a83056ecda1073e92c
Anders <woxxey@gmail.com>
2012-03-26 18:34:14 +0100

    removed new classes again since they do more harm than good cluttering our code

--- src/zeroArgBriansBrain.java
+++
@@ -1,7 +0,0 @@
-public class zeroArgBriansBrain extends Generations {
-
-	public zeroArgBriansBrain() {
-		super(3, "", "2");
-	}
-
-}
--- src/zeroArgCoral.java
+++
@@ -1,6 +0,0 @@
-
-public class zeroArgCoral extends Generations{
-	public zeroArgCoral() {
-		super(2, "45678", "3");
-	}
-}
